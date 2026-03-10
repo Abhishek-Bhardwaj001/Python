13734d298c63fde7fceda74d27c4380ea10aafa0
Abhishek <abhishekbh327@gmail.com>
2026-03-10 18:51:57 +0530

    Added RAG Chatbot.

---
+++ Gen_AI/RAG_Systems/src/history_aware_rag.py
@@ -0,0 +1,50 @@
+from langchain_chroma import Chroma
+from langchain_huggingface import HuggingFaceEmbeddings
+from dotenv import load_dotenv
+import os
+from langchain_groq import ChatGroq
+from langchain_core.messages import SystemMessage, HumanMessage, AIMessage
+from utils.text_formatter import format_query, format_message
+load_dotenv()
+LLM_SECRETS = os.getenv("GROQ_API")
+
+db_directory = "db/chroma_db"
+embedding_model = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+
+db = Chroma(persist_directory=db_directory,
+            embedding_function=embedding_model,
+            collection_metadata={"hnsw:space":"cosine"})
+
+
+retriever = db.as_retriever(search_kwargs = {"k":5})
+
+llm_chatbot = ChatGroq(model='llama-3.1-8b-instant',api_key = LLM_SECRETS) 
+print(f"user Query Example:How much did microsoft pay to acquire github?")
+chat_history = []
+
+def initiate_chat(user_query,chat_history,retriever,chatbot,format_query,format_message):
+            history_text = "\n".join(f"{'User' if isinstance(msg, HumanMessage) else 'Assistant'}: {msg.content}"
+                                                            for msg in chat_history
+                                                        )
+            search_query = format_query(user_query,history_text,chatbot)
+            print(f"re-writen Query:{search_query}")
+            relevant_docs = retriever.invoke(search_query)
+            combined_context = "\n\n".join(doc.page_content for doc in relevant_docs)
+            message = format_message(search_query,history_text,combined_context)
+            response = chatbot.invoke(message)
+            print(f"chatbot Response:\n{response.content}")
+            chat_history.append(HumanMessage(content=user_query))
+            chat_history.append(AIMessage(content=response.content))
+            chat_history = chat_history[-30:]
+
+def main():    
+    while True:
+            user_query = str(input("Please ask your query here:"))
+            if user_query in ['quit','exit','bye']:
+                print("See you, take care!")
+                break
+            else:
+                initiate_chat(user_query,chat_history,retriever,llm_chatbot,format_query,format_message)
+
+if __name__=="__main__":
+      main()       
---
+++ Gen_AI/RAG_Systems/utils/text_formatter.py
@@ -0,0 +1,46 @@
+from langchain_core.messages import SystemMessage, HumanMessage
+def format_query(user_query,chat_history,chatbot):
+    if chat_history:
+        print("Chat history found")
+        message = [SystemMessage(content="""
+                    Your task is to decide whether the user's question depends on previous conversation context.
+
+                    Rules:
+                    - If the question is a follow-up that depends on chat history, rewrite it into a standalone question.
+                    - If the question is already standalone, return it EXACTLY as written.
+                    - Do not use outside knowledge.
+                    - Do not make guess.
+
+                    Do NOT paraphrase standalone questions.
+
+                    Return ONLY the final question text."""),
+                HumanMessage(content = f""" Use the chat history below and rewrite user question.
+                                            Question: {user_query}
+                                            Chat History: {chat_history}
+                                            """)]
+        reformat_result = chatbot.invoke(message)
+        search_query = reformat_result.content.strip()
+    else:
+        print("No Chat history found starting a new session")
+        search_query = user_query
+    return search_query
+
+def format_message(user_query,chat_history,combined_context):
+    if chat_history:
+        print("Chat history found")
+
+        message = [SystemMessage(content="Role: You are an RAG Agent specifically designed to respond to user question based on context and chat history provided to you"),
+                HumanMessage(content = f""" Use the context and chat history below and respond to user question
+                                            Question: {user_query}
+                                            Context: {combined_context}
+                                            Chat History: {chat_history}
+                                            """)]
+    else:
+        print("No Chat history found starting a new session")
+        message = [SystemMessage(content="""Role: You are an RAG Agent specifically designed to respond to user question based on context provided to you.
+                                 Response Format: Respond only with Answer"""),
+        HumanMessage(content = f""" Use the context below and respond to user question
+                                    Question: {user_query}
+                                    Context: {combined_context}
+                                    """)]
+    return message
